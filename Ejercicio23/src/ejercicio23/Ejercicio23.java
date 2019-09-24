fe70302c05685be7e361fcbd0046d6124a368ce3
Luis Miguel <luismrios1602@gmail.com>
2019-09-24 09:50:11 -0500

    Ejercicio 23 - Trabajadores

---
+++ Ejercicio23/src/ejercicio23/Ejercicio23.java
@@ -0,0 +1,24 @@
+/*
+ Una empresa les paga a sus empleados con base en las horas trabajadas en la semana. 
+Para esto, se registran los días que laboró y las horas de cada día. 
+Realice un programa en Java para determinar el sueldo semanal de N trabajadores y además calcule cuánto pagó la empresa por los N empleados.
+ */
+package ejercicio23;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author CBN
+ */
+public class Ejercicio23 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        Scanner leer = new Scanner(System.in);
+    }
+    
+}
