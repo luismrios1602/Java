95adfc24f76fd5e7d85d84087552280864bef19b
Luis Miguel <luismrios1602@gmail.com>
2019-09-24 10:57:07 -0500

    Factorial - Actualizado (Sin terminar)

--- Factorial/src/factorial/Factorial.java
+++ Factorial/src/factorial/Factorial.java
@@ -5,6 +5,8 @@
  */
 package factorial;
 
+import java.util.Scanner;
+
 /**
  *
  * @author CBN
@@ -16,6 +18,20 @@ public class Factorial {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        Scanner leer = new Scanner(System.in);
+        int num, fact=1, acum;
+        System.out.print("Ingrese el número: ");
+        num = leer.nextInt();
+        
+        //Ciclo para mostrar el final del factial:
+        for (int i = 1; i <= num; i++) {
+            //Ciclo para hallar del factorial en base al número por donde va:
+            
+           for (int j = i; j <= 1; j--) {
+                fact = (fact) * j;
+            }
+            System.out.println(i+"! = "+fact);
+        }
     }
     
 }
